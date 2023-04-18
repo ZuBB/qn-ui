f481584fea803777d09de7787d95165b5d693be1
Vasyl Zuziak <zvasylvv@gmail.com>
2023-04-18 11:33:54 -0700

    feat: check in complete version of <AppContainer>

--- src/AppContainer.tsx
+++ src/AppContainer.tsx
@@ -1,24 +1,44 @@
-import React from 'react';
+import React, { useEffect, useCallback } from 'react';
 import { Box, Flex, Spacer } from '@chakra-ui/react';
+import { useSearchParams } from 'react-router-dom';
 import { AppHeader } from './AppHeader';
 import { IncompletedTodos } from './IncompletedTodos';
 import { CompletedTodos } from './CompletedTodos';
 import { AddTodo } from './AddTodo';
 import { SearchTodos } from './SearchTodos';
+import { useTodoStore } from './store'
+import { loadTodos } from './loadTodos'
 
 export const AppContainer = () => {
+  const setTodos = useTodoStore(state => state.setTodos);
+  const [searchParams] = useSearchParams();
+
+  const getTodos = useCallback(
+    async () => {
+      try {
+        setTodos(await loadTodos());
+      } catch (error) {
+        console.log('Error happened', error);
+      }
+    },
+    [setTodos]
+  );
+
+  useEffect(() => { getTodos() }, [getTodos])
+  useEffect(() => { getTodos() }, [getTodos, searchParams])
+
   return (
     <Box
-      w="40%"
-      ml="30%"
-      mr="30%"
+      w="50%"
+      ml="25%"
+      mr="25%"
       mt="10% !important"
-      bgColor="lightgray"
+      bgColor="#F7FAFC"
       padding="20px"
       borderRadius="md"
     >
       <AppHeader />
-      <Flex direction="row" mt="10" w="100%" >
+      <Flex direction="row" mt="10">
         <Flex direction="column" w="48%">
           <AddTodo />
           <IncompletedTodos />
@@ -29,7 +49,6 @@ export const AppContainer = () => {
           <CompletedTodos />
         </Flex>
       </Flex>
-
     </Box>
   );
 };
