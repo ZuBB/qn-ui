cf74b3bed0abf1655e4e60ea106dc3ba407a953b
Vasyl Zuziak <zvasylvv@gmail.com>
2023-04-18 11:56:26 -0700

    feat: add full implementation of <AddTodo> component

--- src/AddTodo.tsx
+++ src/AddTodo.tsx
@@ -1,9 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Flex, FormControl, Input } from '@chakra-ui/react';
+import axios from 'axios';
+import { useTodoStore } from './store'
+import { loadTodos } from './loadTodos'
 
-export const AddTodo = () => (
-  <FormControl as={Flex} verticalAlign="center" mb="10">
-    <Input name="todo" placeholder="Enter todo" required />
-    <Button ml="3">Add</Button>
-  </FormControl>
-);
+const addTodoUrl = '/api/todos'
+
+export const AddTodo = () => {
+  const [todo, setTodo] = useState('');
+  const setTodos = useTodoStore(state => state.setTodos);
+
+  const onChangeHandler = (event: React.FormEvent<HTMLInputElement>) => {
+    setTodo(event.currentTarget.value)
+  }
+
+  const onKeyDownHandler: React.KeyboardEventHandler<HTMLInputElement> = (event) => {
+    if (event.key === 'Enter') {
+      createTodo();
+    }
+  }
+
+  const onCreateHandler = () => createTodo();
+
+  const createTodo = async () => {
+    try {
+      await axios.post(addTodoUrl, { todo })
+      setTodos(await loadTodos())
+      setTodo('')
+    } catch (error) {
+      console.log('Error happened', error);
+    }
+  }
+
+  return (
+    <FormControl as={Flex} verticalAlign="center" mb="10">
+      <Input
+        name="todo"
+        placeholder="Enter todo"
+        required
+        value={todo}
+        onChange={onChangeHandler}
+        onKeyDown={onKeyDownHandler}
+      />
+      <Button ml="3" onClick={onCreateHandler}>Add</Button>
+    </FormControl>
+  );
+}
