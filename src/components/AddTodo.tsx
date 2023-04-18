b3a9aade4dc35c1d7f50a91441bd98aef03d03c1
Vasyl Zuziak <zvasylvv@gmail.com>
2023-04-18 14:37:01 -0700

    chore: move also <AddTodo>
    [body]
    
    [footer]

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -1,34 +1,39 @@
 import React, { useState } from 'react';
-import { Button, Flex, FormControl, Input } from '@chakra-ui/react';
+import { Button, Flex, FormControl, Input, useToast } from '@chakra-ui/react';
 import axios from 'axios';
-import { useTodoStore } from './store'
-import { loadTodos } from './loadTodos'
+import { useTodoStore } from '../store'
+import { loadTodos } from '../loadTodos'
+import { getMessageFromBeResonse } from '../utils'
+
 
 const addTodoUrl = '/api/todos'
 
 export const AddTodo = () => {
+  const toast = useToast();
   const [todo, setTodo] = useState('');
   const setTodos = useTodoStore(state => state.setTodos);
 
   const onChangeHandler = (event: React.FormEvent<HTMLInputElement>) => {
-    setTodo(event.currentTarget.value)
+    setTodo(event.currentTarget.value);
   }
 
   const onKeyDownHandler: React.KeyboardEventHandler<HTMLInputElement> = (event) => {
     if (event.key === 'Enter') {
       createTodo();
     }
-  }
+  };
 
   const onCreateHandler = () => createTodo();
 
   const createTodo = async () => {
     try {
       await axios.post(addTodoUrl, { todo })
-      setTodos(await loadTodos())
+      toast({ status: 'success', title: 'To do created!' })
       setTodo('')
+
+      setTodos(await loadTodos())
     } catch (error) {
-      console.log('Error happened', error);
+      toast({ status: 'error', title: getMessageFromBeResonse(error) })
     }
   }
 
